b37d852e1e44bf01ee5502b75519e57b2fe94042
Rob King <rob.king@volparahealth.com>
2024-02-08 11:43:06 +1300

    Add project files.

---
+++ SystemExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace System;
+
+public static class SystemExtensions
+{
+    public static bool HasValue(this string input) => !string.IsNullOrEmpty(input);
+
+    public static bool IsEmpty(this string input) => string.IsNullOrEmpty(input);
+}
